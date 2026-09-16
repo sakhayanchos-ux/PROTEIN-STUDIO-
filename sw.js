91c8cad29695a65fd8e39148b8c8d59a85ae9023
sakhayanchos-ux <sakhayanchos@gmail.com>
2026-09-16 16:41:50 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='protein-studio-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return r}).catch(()=>caches.match('./index.html'))))});
