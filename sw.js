0806b8364685374242173850b6b67e66dc2275ca
sakhayanchos-ux <sakhayanchos@gmail.com>
2026-09-16 16:40:49 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='protein-studio-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return r}).catch(()=>caches.match('./index.html'))))});
